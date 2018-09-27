1fb96049134706513ed9dfed1d09a2a1e6e07cd0
Jonatan Buitrago <jobuit20@gmail.com>
2018-09-27 11:34:54 -0500

    version 1.2

--- src/app/componentes/informacion-familiar/informacion-familiar.component.ts
+++ src/app/componentes/informacion-familiar/informacion-familiar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {InformacionFamiliar} from '../../modelos/informacion-familiar';
+import {InformacionFamiliar, DatosDependen} from '../../modelos/informacion-familiar';
+import { MetodosService } from '../../servicios/metodos.service';
+import { FirebaseCrudService } from '../../servicios/firebase-crud.service';
 
 @Component({
   selector: 'app-informacion-familiar',
@@ -10,9 +12,37 @@ export class InformacionFamiliarComponent implements OnInit {
 
   informacionFamiliarObject:InformacionFamiliar=new InformacionFamiliar();
 
-  constructor() { }
+  constructor(public metodoService:MetodosService,public firebaseCrud:FirebaseCrudService) { }
 
   ngOnInit() {
-    this.informacionFamiliarObject.numero_personas_dependen=0;
+    this.firebaseCrud.setOpcion('informacion-familiar');
+
+    this.ConseguirDatosFirebase();
+  }
+
+  onChangeDependenSelect(){
+    /*for(let i=0;i<this.informacionFamiliarObject.numero_personas_dependen;i++){
+      this.informacionFamiliarObject.dependen.push(new DatosDependen('','',''));
+    }*/
+    
+  }
+
+  ConseguirDatosFirebase(){
+    this.firebaseCrud.TomarObjeto().valueChanges()
+    .subscribe(res => {
+      if(res!=null){
+        this.informacionFamiliarObject = res as InformacionFamiliar;
+        console.log(this.informacionFamiliarObject.dependen)
+      }
+    });
+  }
+
+  SaveInformationFamiliar(){
+    for(let i=0;i<this.informacionFamiliarObject.numero_personas_dependen;i++){
+      console.log(this.informacionFamiliarObject.dependen[i].nombres_parentesco);
+    }
+
+    this.firebaseCrud.CrearNuevoObjeto(this.informacionFamiliarObject);
+    this.metodoService.MensajeFlash('Informacion guardada correctamente','','success');
   }
 }
--- src/app/modelos/informacion-familiar.ts
+++ src/app/modelos/informacion-familiar.ts
@@ -9,26 +9,30 @@ export class InformacionFamiliar {
     ciudad_esposa:string;
 
     numero_personas_dependen:number;
-    nombres_parentesco:string;
-    parentesco:string;
-    edad:string;
+
+    dependen: DatosDependen[] = [];
+
     nombre_padre:string;
     profesion_ocupacion_padre:string;
     telefono_padre:string;
     nombre_madre:string;
     profesion_ocupacion_madre:string;
     telefono_madre:string;
-    tiene_hermanos:string;
+
+    numero_hermanos:string;
     nombre_hermano:string;
     profesion_ocupacion_hermano:string;
     telefono_hermano:string;
-    nombre_hermano2:string;
-    profesion_ocupacion_hermano2:string;
-    telefono_hermano2:string;
-    nombre_hermano3:string;
-    profesion_ocupacion_hermano3:string;
-    telefono_hermano3:string;
-    nombre_hermano4:string;
-    profesion_ocupacion_hermano4:string;
-    telefono_hermano4:string;
+
+}
+
+export class DatosDependen{
+    nombres_parentesco:string;
+    parentesco:string;
+    edad:string;
+    constructor(nombres_parentesco:string, parentesco:string,edad:string){
+        this.nombres_parentesco=nombres_parentesco;
+        this.parentesco=parentesco;
+        this.edad=edad;  
+    }
 }
